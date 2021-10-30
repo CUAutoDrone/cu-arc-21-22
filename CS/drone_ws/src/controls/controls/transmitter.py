a09bb3ba32c7f923d73707ac96fe9edf7236dd4d
Clay Goodling <jcg284@cornell.edu>
2021-10-30 17:55:36 -0400

    ros2 arm transmitter node

--- CS/drone_ws/src/controls/controls/transmitter.py
+++ CS/drone_ws/src/controls/controls/transmitter.py
@@ -1,59 +1,68 @@
-from fccontrolclass import FlightControllerCommands as fc
-import rospy
+from .fccontrolclass import FlightControllerCommands as fc
 from threading import Thread
 from time import sleep
+import rclpy
+from rclpy.executors import Executor
+from rclpy.node import Node
+from std_msgs.msg import Boolean, Int
 
 
-def arm_transmitter(fc):
-    rospy.init_node('arm_transmitter', anonymous=True)
-    rospy.Subscriber('arm', bool, lambda b : fc.arm() if b else fc.disarm())
-    rospy.spin()
+# def main(args=None):
+#     rclpy.init(args=args)
+#     node = Node('my_node_name')
+#     rclpy.spin(node)
+#     rclpy.shutdown()
+# if __name__ == '__main__':
+#     main()
 
 
-def throttle_transmitter(fc):
-    rospy.init_node('arm_transmitter', anonymous=True)
-    rospy.Subscriber('throttle', int, lambda i : fc.setThrottle(i))
-    rospy.spin()
+class ArmTransmitter(Node):
+    def __init__(self, fc):
+        super.__init__('arm_transmitter')
+        self.subscription = self.create_subscription(
+            Boolean, 'arm', self.callback, 1
+        )
+        self.fc = fc
+
+    def callback(self, msg):
+        if msg:
+            self.fc.arm()
+        else:
+            self.fc.disarm()
 
 
-def pitch_transmitter(fc):
-    rospy.init_node('arm_transmitter', anonymous=True)
-    rospy.Subscriber('pitch', int, lambda i : fc.setPitch(i))
-    rospy.spin()
+# def throttle_transmitter(fc):
+#     rospy.init_node('arm_transmitter', anonymous=True)
+#     rospy.Subscriber('throttle', int, lambda i : fc.setThrottle(i))
+#     rospy.spin()
 
 
-def roll_transmitter(fc):
-    rospy.init_node('arm_transmitter', anonymous=True)
-    rospy.Subscriber('roll', int , lambda i : fc.setRoll(i))
-    rospy.spin()
+# def pitch_transmitter(fc):
+#     rospy.init_node('arm_transmitter', anonymous=True)
+#     rospy.Subscriber('pitch', int, lambda i : fc.setPitch(i))
+#     rospy.spin()
 
 
-def yaw_transmitter(fc):
-    rospy.init_node('arm_transmitter', anonymous=True)
-    rospy.Subscriber('yaw', int, lambda i : fc.setYaw(i))
-    rospy.spin()
+# def roll_transmitter(fc):
+#     rospy.init_node('arm_transmitter', anonymous=True)
+#     rospy.Subscriber('roll', int , lambda i : fc.setRoll(i))
+#     rospy.spin()
+
+
+# def yaw_transmitter(fc):
+#     rospy.init_node('arm_transmitter', anonymous=True)
+#     rospy.Subscriber('yaw', int, lambda i : fc.setYaw(i))
+#     rospy.spin()
      
 
-def main():
+def main(args=None):
     control = fc()
     t1 = Thread(target=control.run)
     t1.start()
 
-    arm = Thread(target=arm_transmitter, args=(control,))
-    arm.start()
-
-    throttle = Thread(target=throttle_transmitter, args=(control,))
-    throttle.start()
-
-    pitch = Thread(target=pitch_transmitter, args=(control,))
-    pitch.start()
-
-    roll = Thread(target=roll_transmitter, args=(control,))
-    roll.start()
-    
-    yaw = Thread(target=yaw_transmitter, args=(control,))
-    yaw.start()
-
+    executor = Executor()
+    executor.add_node(ArmTransmitter(control))
+    executor.spin()
 
 if __name__ == '__main__':
     main()
