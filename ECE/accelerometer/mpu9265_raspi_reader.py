16442e5d46f730ed27f5589434a206d7dcf3e2e7
wihow21 <wihowes2003@gmail.com>
2022-04-16 16:32:23 -0400

    Update mpu9265_raspi_reader.py

--- ECE/accelerometer/mpu9265_raspi_reader.py
+++ ECE/accelerometer/mpu9265_raspi_reader.py
@@ -10,6 +10,10 @@
 #
 #  FaBo <info@fabo.io>
 
+
+# Accelerometer reader that outputs pitch, roll and yaw.
+# Pitch and roll are accurate, but yaw doesn't react to movement
+
 import FaBo9Axis_MPU9250
 import time
 import sys
@@ -20,16 +24,19 @@ mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 try:
     while True:
 
+        # raw accelerometer data 
         accel = mpu9250.readAccel()
         #print(" ax = " , ( accel['x'] ))
         #print(" ay = " , ( accel['y'] ))
         #print(" az = " , ( accel['z'] ))
 
+        # raw gyroscope data
         gyro = mpu9250.readGyro()
         #print(" gx = " , ( gyro['x'] ))
         #print(" gy = " , ( gyro['y'] ))
         #print(" gz = " , ( gyro['z'] ))
 
+        # raw magnetic data
         mag = mpu9250.readMagnet()
         #print(" mx = " , ( mag['x'] ))
         #print(" my = " , ( mag['y'] ))
@@ -45,14 +52,16 @@ try:
         magY = mag['y']
         magZ = mag['z']
 
+        # convert accelerometer data into pitch and roll
         pitch = atan2(accelY ,( sqrt ((accelX * accelX) + (accelZ * accelZ))))
         roll = atan2(-accelX ,( sqrt((accelY * accelY) + (accelZ * accelZ))))
 
+        # convert magnetic data, pitch, and roll into yaw
         Yh = (magY * cos(roll)) - (magZ * sin(roll))
         Xh = (magX * cos(pitch)) + (magY * sin(roll)*sin(pitch)) + (magZ * cos(roll) * sin(pitch))
-
-
         yaw =  atan2(Yh, Xh)
+        
+        # Convert to degrees
         roll = roll*57.3
         pitch = pitch*57.3
         yaw = yaw*57.3
