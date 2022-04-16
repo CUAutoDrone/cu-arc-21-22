43716bb7fd3d9dea2765d2d5b4d19f9941773310
newt-powers <cjp245@cornell.edu>
2022-04-16 17:52:47 -0400

    Update mechie_test_rig code with better comments

--- ECE/Mechie Motor Testing Rig/mechie_test_rig/mechie_test_rig.ino
+++ ECE/Mechie Motor Testing Rig/mechie_test_rig/mechie_test_rig.ino
@@ -1,41 +1,37 @@
 #include <Servo.h>
-int value = 1000; // hold the value which will be sent to the motor
-//int test = 0; //dummy variable to make sure the incoming message is legit
-int pot_val = 0;
+int throttle = 1000; //Hold the value which will be sent to the motor
 
-Servo firstESC; //Creating motor object instance
+int pot_val = 0; //The amount the potentiometer is turned
+
+Servo firstESC; //Create the motor object instance
 
 void setup() {
-  firstESC.attach(11);    // attached to pin 11 I just do this with 1 Servo
-  Serial.begin(9600);    // start serial at 9600 baud
+  firstESC.attach(11); //Attach the servo motor to pin 11
+  Serial.begin(9600); //Start serial at 9600 baud
   delay(2000);
   Serial.println("Serial ready:");
-  //firstESC.writeMicroseconds(1000);
-  arm(firstESC);
+  arm(firstESC); //Begin the arming process
 }
 
 void loop() {
-  pot_val = analogRead(A0);//reads analog value from potentiometer 
-  //maps analog value which ranges from 0 to 990 to the pwm values which are 1100 2000;
-  value = map(pot_val, 0, 990, 1100, 2000);
-//   Serial.println(value);
-  //Write value to motor
-  firstESC.write(value);
-  //1100 is minimum
-  //maximum???
+  pot_val = analogRead(A0); //Read the analog value from the potentiometer 
+  //Map the analog value which range from 0 to 990 to the pwm values which are 1100 to 2000;
+  throttle = map(pot_val, 0, 990, 1100, 2000);
+  //Write the value to the motor
+  firstESC.write(throttle);
 }
 
-//function to arm the motor
+//Function to arm the motor
 void arm(Servo ESC) {
   Serial.print("Arming");
-  ESC.write(2000);
-  ESC.write(1000);
+  ESC.write(2000); //Send repeated signals of full throttle
+  ESC.write(1000); //to no throttle for the startup sequence
   Serial.print("Arming");
-  ESC.write(1000); //start
+  ESC.write(1000);
   delay(100);
-  ESC.write(1000); //value at which the motor starts turning
+  ESC.write(1000);
   delay(2000);
-  ESC.write(1000); //value at which the motor stands still
+  ESC.write(1000);
   delay(3000);
   Serial.println(" Armed!");
 }
