0723f484e79da63f52781c04f0a0092b6bed6f3e
newt-powers <cjp245@cornell.edu>
2022-04-16 17:05:27 -0400

    Comment DroneChannelExercise Code

--- ECE/DroneChannelExercise/DroneChannelExercise.ino
+++ ECE/DroneChannelExercise/DroneChannelExercise.ino
@@ -1,6 +1,5 @@
 #include <SoftwareSerial.h>
 
-//hello
 int roll = 1000; //1500 is level, no tilt
 int pitch = 1000;
 int throttle = 1000;
@@ -14,13 +13,14 @@ int sum = 0;
 SoftwareSerial mySerial(0, 1); // RX, TX
 
 void setup() {
-  // put your setup code here, to run once:
-  mySerial.begin(115200);
-  //Serial.begin(115200);
+  // 
+  mySerial.begin(115200); 
+  
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Native USB only
+    ; //Wait for serial port to connect. Needed for Native USB only
   }
-  //Goal: increase yaw by 20
+
+  //Set the first 4 channels to the integer values above
   drone_channels[0] = roll;
   drone_channels[1] = pitch;
   drone_channels[2] = throttle;
@@ -30,32 +30,36 @@ void setup() {
   for(int i = 4; i<14; i++){
     drone_channels[i] = empty;
   }
+  
   //Send starting bytes; 0x40 is most significant 
   //even though it is second because of little endian order
   sending_message[0] = 0x20;
   sending_message[1] = 0x40;
-  //looks like something is transmitting, but the starting bytes are incorrect?
-  
+
+  //Send all of the bytes from the drone channels
   for(int i = 2; i<=(sizeof(drone_channels)/4)*2 - 2; i+=2){
     sending_message[i] = drone_channels[i-2]%256;
     sending_message[i+1] = drone_channels[i-2]/256;
    }
+   
   //Sum the bytes in sending_message
   sum = 0;
   for(int i = 0; i <= sizeof(sending_message) - 2; i+=1) {
     sum += sending_message[i];
   }
-  //Put in little endian
+  
+  //Put in little endian order
   checksum = 0xFFFF - sum;
   sending_message[sizeof(sending_message)-2] = checksum%256;
   sending_message[sizeof(sending_message)-1] = checksum/256;
+
+  //Send each of the bytes in sending_message
+  for(int i=0; i < 32; i++) {
+      mySerial.write(sending_message[i]);
+  }
+   delay(7);
 }
 
 void loop() {
-  //Send each of the bytes in sending_message
-  for(int i=0; i < 1; i++) {
-    //if(mySerial.available())
-      mySerial.write(sending_message[i]);
-  }
-  delay(7);
+  
 }
