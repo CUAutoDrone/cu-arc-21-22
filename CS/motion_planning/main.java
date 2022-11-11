fd4a380b708178f5f03675dd532f6aa562da5925
Dina <db676@cornell.edu>
2022-11-10 20:10:25 -0500

    A* debugging:

--- CS/motion_planning/main.java
+++ CS/motion_planning/main.java
@@ -1,58 +1,17 @@
 package astar;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
-    public static double aStar(int[][] graph, double[][] heuristic, int start, int goal) {
-        int[] distances = new int[graph.length];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[start] = 0;
-
-        double[] priorities = new double[graph.length];
-        Arrays.fill(priorities, Integer.MAX_VALUE);
-        priorities[start] = heuristic[start][goal];
-
-        boolean[] visited = new boolean[graph.length];
-
-        while (true) {
-
-            double lowestPriority = Integer.MAX_VALUE;
-            int lowestPriorityIndex = -1;
-            for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] < lowestPriority && !visited[i]) {
-                    lowestPriority = priorities[i];
-                    lowestPriorityIndex = i;
-                }
-            }
-
-            if (lowestPriorityIndex == -1) {
-                return -1;
-            } else if (lowestPriorityIndex == goal) {
-                System.out.println("Goal node found!");
-                return distances[lowestPriorityIndex];
-            }
-
-            System.out.println("Visiting node " + lowestPriorityIndex + " with currently lowest priority of " + lowestPriority);
-
-            for (int i = 0; i < graph[lowestPriorityIndex].length; i++) {
-                if (graph[lowestPriorityIndex][i] != 0 && !visited[i]) {
-                    if (distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i] < distances[i]) {
-                        distances[i] = distances[lowestPriorityIndex] + graph[lowestPriorityIndex][i];
-                        priorities[i] = distances[i] + heuristic[i][goal];
-                        System.out.println("Updating distance of node " + i + " to " + distances[i] + " and priority to " + priorities[i]);
-                    }
-                }
-            }
-            visited[lowestPriorityIndex] = true;
-            System.out.println("Visited nodes: " + Arrays.toString(visited));
-            System.out.println("Currently lowest distances: " + Arrays.toString(distances));
-
-        }
-    }
-    
-    public static void main(String[] args) {
-    	int[][] graph = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-    	double[][] heuristic = {{Integer.MAX_VALUE, 1, 1}, {1, Integer.MAX_VALUE, 1}, {1, 1, Integer.MAX_VALUE}};
-    	System.out.println("A* result: " + aStar(graph, heuristic, 0, 2));
-    }
+	public static void main(String[] args) {
+		ArrayList<Node> graph = new ArrayList<Node>();
+		Node n1 = new Node(1, 0, 0, new ArrayList<Node>());
+		graph.add(n1);
+		ArrayList<Node> n2Neighbors = new ArrayList<Node>();
+		n2Neighbors.add(n1);
+		Node n2 = new Node(2, 1, 1, n2Neighbors);
+		graph.add(n2);
+		AStar aStar = new AStar(graph, n1, n2);
+		System.out.println(aStar.search());
+	}
 }
