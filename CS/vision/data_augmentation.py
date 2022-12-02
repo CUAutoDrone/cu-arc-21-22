7f4c1dab3863aa498634b8aa72fe2159a33edc56
gracege678 <71044823+gracege678@users.noreply.github.com>
2022-12-01 20:50:21 -0500

    Added scaling and refactored code

--- CS/vision/data_augmentation.py
+++ CS/vision/data_augmentation.py
@@ -11,20 +11,17 @@ def imread(filename):
     img = img.convert('RGB')
     img = np.array(img)
     img = img.astype(float)/255
-    
     return img
 
 def rotate_pixel(x, y, cx, cy, theta):
-    tempX = x - 180
-    tempY = y - 320
-    print(x, y, tempX, tempY)
+    tempX = x - cx
+    tempY = y - cy
     theta = theta * math.pi / 180
     rotatedX = tempX * np.cos(theta) - tempY * np.sin(theta)
     rotatedY = tempX * np.sin(theta) + tempY * np.cos(theta)
-    return [rotatedX + 180, rotatedY + 320]
+    return [rotatedX + cx, rotatedY + cy]
 
-def newxml(file_num,item_type, shiftx = None, shifty = None, theta = None):
-    file_name = 'mast_' + str(file_num) + '.png'
+def newxml(file_num,item_type, shiftx = None, shifty = None, theta = None, factor = None):
     new_file_name = 'mast_' + item_type + str(file_num) + '.png'
     xml_name = 'mast_' + str(file_num) + '.xml'
     new_xml_name = 'mast_' + item_type + str(file_num) + '.xml'
@@ -33,18 +30,38 @@ def newxml(file_num,item_type, shiftx = None, shifty = None, theta = None):
     root = tree.getroot()
     for filename in root.iter('filename'):
         filename.text = new_file_name
+    
     for path in root.iter('path'):
         old = path.text
         i = old.rfind('/')
         path.text = old[:i + 1] + new_file_name
-    # if we need to translate bounding box:
+
+    # Get height and width of image
+    for size in root.iter('size'):
+        for width in root.iter('width'):
+            max_width = int(width.text)
+        for height in root.iter('height'):
+            max_height = int(height.text)
+    
+    # Get center of images
+    cx = max_width // 2
+    cy = max_height // 2
+
+    # Get points of bounding box
+    for obj in root.iter('object'):
+        for name in root.iter('name'):
+            for bndbox in root.iter('bndbox'):
+                for xmin in root.iter('xmin'):
+                    x1 = int(xmin.text)
+                for xmax in root.iter('xmax'):
+                    x2 = int(xmax.text)
+                for ymin in root.iter('ymin'):
+                    y1 = int(ymin.text)
+                for ymax in root.iter('ymax'):
+                    y2 = int(ymax.text)
+
+    # Translate
     if shiftx:
-        print("shift")
-        for size in root.iter('size'):
-            for width in root.iter('width'):
-                max_width = int(width.text)
-            for height in root.iter('height'):
-                max_height = int(height.text)
         for obj in root.iter('object'):
             for name in root.iter('name'):
                 for bndbox in root.iter('bndbox'):
@@ -60,66 +77,48 @@ def newxml(file_num,item_type, shiftx = None, shifty = None, theta = None):
                     for ymax in root.iter('ymax'):
                         val = min(max((int(ymax.text) + shifty), 0), max_height)
                         ymax.text = str(val)
-    # if rotation
+   
+   # Rotate
     if theta:
-        print("rotate", theta)
-        for size in root.iter('size'):
-            for width in root.iter('width'):
-                max_width = int(width.text)
-            for height in root.iter('height'):
-                max_height = int(height.text)
-
         minx = max_width
         miny = max_height
         maxx = 0
         maxy = 0
-        cx = max_width // 2
-        cy = max_height // 2
-
-        # M = cv2.getRotationMatrix2D((180, 320), -45, 1.0)
-        # M_inv = cv2.getRotationMatrix2D((cx, cy), -45, 1.0)
-
-        for obj in root.iter('object'):
-            for name in root.iter('name'):
-                for bndbox in root.iter('bndbox'):
-                    for xmin in root.iter('xmin'):
-                        x1 = int(xmin.text)
-                    for xmax in root.iter('xmax'):
-                        x2 = int(xmax.text)
-                    for ymin in root.iter('ymin'):
-                        y1 = int(ymin.text)
-                    for ymax in root.iter('ymax'):
-                        y2 = int(ymax.text)
-        # points = np.array([[[x1, y1], [x1, y2], [x2, y2], [x2, y1]]], dtype=np.float32)
-        # ones = np.ones(shape=(len(points), 1))
-        # points_ones = np.hstack([points, ones])
-        # lst = cv2.warpAffine(points, M, (max_width, max_height))
-
-        # lst = cv2.transform(points, M)[0]
-        # 
-        # 
-        # # transform points
-        # transformed_points = M_inv.dot(points_ones.T).T
-        # lst = transformed_points
 
         lst = []
-        lst.append(rotate_pixel(x1, y1, cx, cy, theta))
-        print(theta)
-        print(x1, y1)
-        print(rotate_pixel(x1, y1, cx, cy, theta))
-        lst.append(rotate_pixel(x1, y2, cx, cy, theta))
-        
-        lst.append(rotate_pixel(x2, y1, cx, cy, theta))
-        print(lst[2])
-        lst.append(rotate_pixel(x2, y2, cx, cy, theta))
-        print(lst)
+        for xi in [x1, x2]:
+            for yi in [y1, y2]:
+                lst.append(rotate_pixel(xi, yi, cx, cy, theta))
+
         for i in lst:
             minx = min(i[0], minx)
             maxx = max(i[0], maxx)
             miny = min(i[1], miny)
             maxy = max(i[1], maxy)
-        print(minx, maxx, miny, maxy)
         
+        for obj in root.iter('object'):
+            for name in root.iter('name'):
+                for bndbox in root.iter('bndbox'):
+                    for xmin in root.iter('xmin'):
+                        val = min(max(minx, 0), max_width)
+                        xmin.text = str(val)
+                    for xmax in root.iter('xmax'):
+                        val = min(max(maxx, 0), max_width)
+                        xmax.text = str(val)
+                    for ymin in root.iter('ymin'):
+                        val = min(max(miny, 0), max_height)
+                        ymin.text = str(val)
+                    for ymax in root.iter('ymax'):
+                        val = min(max(maxy, 0), max_height)
+                        ymax.text = str(val)
+
+    # Scale
+    if factor:
+        minx = (x1 - cx) * factor + cx
+        miny = (y1 - cy) * factor + cy
+        maxx = (x2 - cx) * factor + cx
+        maxy = (y2 - cy) * factor + cy
+
         for obj in root.iter('object'):
             for name in root.iter('name'):
                 for bndbox in root.iter('bndbox'):
@@ -186,9 +185,39 @@ def rotate(file_num, theta):
     # new xml
     newxml(file_num, '02', None, None, -theta)
 
-def scale(file_num, theta):
-    oass
+def scale(file_num, factor):
+    file_name = 'mast_' + str(file_num) + '.png'
+    new_file_name = 'mast_03' + str(file_num) + '.png'
+    # Read image
+    img = cv2.imread(file_name) 
+    
+    w = 360
+    h = 640
 
+    num_rows, num_cols = img.shape[:2]
+    dim = (int(w * factor), int(h * factor)) #dimensions of new image
+
+    # Scale up
+    if factor > 1:
+        img_rotation = cv2.resize(img, dim, interpolation = cv2.INTER_LINEAR)
+        mid_h = int(h // 2 * factor)
+        mid_w = int(w // 2 * factor)
+        # Crop image to original size
+        img_rotation = img_rotation[mid_h - h //2 : mid_h + h // 2, mid_w - w // 2 : mid_w + w // 2]
+    # Scale down
+    else:
+        img_rotation = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        top = (h - int(h * factor)) // 2
+        bottom = (h - int(h * factor)) // 2 + (h - int(h * factor)) % 2
+        left = (w - int(w * factor)) // 2
+        right = (w - int(w * factor)) // 2 + (w - int(w * factor)) % 2
+        color = list(cv2.mean(img))[:3] #average color
+        # Pad image to original size
+        img_rotation = cv2.copyMakeBorder(img_rotation, top, bottom, left, right, cv2.BORDER_CONSTANT,value=color) 
+    # new picture
+    cv2.imwrite(new_file_name, img_rotation)
+    # new xml
+    newxml(file_num, '03', factor = factor)
 
 if __name__ == "__main__":
     # for i in range(556):
@@ -197,8 +226,7 @@ if __name__ == "__main__":
     #         rotate(file_num)
     #     except:
     #         pass
-    # blur(552)
-    # translate(552, 123, 29)
+    blur(552)
+    translate(552, 123, 29)
     rotate(552, 10)
-    # scale(552, )
-   
+    scale(552, 0.75)
