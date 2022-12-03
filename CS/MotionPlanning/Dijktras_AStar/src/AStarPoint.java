d25197023d16b4d84a34ac9fb8f71bd175042877
JT-Klenke <116977579+JT-Klenke@users.noreply.github.com>
2022-12-03 14:49:10 -0500

    Update AStarPoint.java

--- CS/MotionPlanning/Dijktras_AStar/src/AStarPoint.java
+++ CS/MotionPlanning/Dijktras_AStar/src/AStarPoint.java
@@ -1,13 +1,21 @@
 public class AStarPoint extends Point{
-    int hCost;
-    int gCost;
+    public int hCost;
+    public int gCost = Integer.MAX_VALUE/3*2;
 
-    AStarPoint(int x, int y){
+    /**
+     * Creates an AStarPoint at the coordinate x,y
+     */
+    public AStarPoint(int x, int y){
         super(x,y);
     }
 
     public int fCost(){
         return hCost + gCost;
+    }
+
+    @Override
+    public String toString(){
+        return "x,y: " + super.getX() + "," + super.getY() + " hCost: " + hCost + " gCost: "+gCost;
     }
 
 }
